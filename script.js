132d9c05e12747d28bc62ef9b9953d8ef4f92a31
3nzo <3nzo@mbp-de-di.home>
2024-01-22 00:38:43 +0100

    Ajout JS et modif HTML/CSS

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
+// Loader
 const loader = document.querySelector(".loader");
 
 window.addEventListener("load", () => {
   loader.classList.add("fondu-out");
 });
 
+// toTopBtn
 const upBtn = document.querySelector(".btnUp");
+
+window.addEventListener("scroll", (event) => {
+  if (window.scrollY > 300) {
+    upBtn.classList.add("show");
+  } else {
+    upBtn.classList.remove("show");
+  }
+});
 
 upBtn.addEventListener("click", () => {
   window.scrollTo({
@@ -14,6 +24,7 @@ upBtn.addEventListener("click", () => {
   });
 });
 
+// Text présentation
 const element = document.getElementById("animated-text");
 const words = ["Enzo Di Giovanni.", "un développeur !"];
 let currentWordIndex = 0;
@@ -51,3 +62,42 @@ function updateText() {
 
 // Initialiser l'animation
 setTimeout(updateText, 200);
+
+// Récuperer les nav-link
+// Écouter quand un nav-link est clické
+// Récuperer l'href du nav-link clické et trouver la section qui à ce href (ID de la section)
+// Scroll jusqu'à l'élement (scroll to)
+
+const navLinks = document.querySelectorAll(".nav-link");
+const navHeight = document.getElementById("navHeader").offsetHeight;
+
+navLinks.forEach((navLink) => {
+  navLink.addEventListener("click", (e) => {
+    e.preventDefault();
+    const sectionId = navLink.getAttribute("href");
+    const section = document.querySelector(sectionId);
+    window.scrollTo({
+      top: section.offsetTop - navHeight,
+      left: 0,
+      behavior: "smooth",
+    });
+    console.log(section.offsetTop);
+  });
+});
+
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+  sections.forEach((section) => {
+    if (scrollY >= section.offsetTop - navHeight) {
+      const activeNavLink = document.querySelector(".nav-link.active");
+      if (activeNavLink) {
+        activeNavLink.classList.remove("active");
+      }
+      const navLink = document.querySelector(
+        ".nav-link[href='#" + section.id + "']"
+      );
+      navLink.classList.add("active");
+    }
+  });
+});
