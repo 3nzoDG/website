865265fbee659a1b631c120bfa84946a906e0f38
3nzo <3nzo@mbp-de-di.home>
2024-03-11 20:27:21 +0100

    Responsive + BurgerMenu & Modifi JS

--- script.js
+++ script.js
@@ -5,6 +5,20 @@ window.addEventListener("load", () => {
   loader.classList.add("fondu-out");
 });
 
+// Burger Btn
+const links = document.querySelectorAll("nav li");
+const icons = document.querySelector("#icons");
+const nav = document.querySelector("#navHeader");
+icons.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
+});
+
 // toTopBtn
 const upBtn = document.querySelector(".btnUp");
 
