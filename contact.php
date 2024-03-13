9c6d132db357c70fdfdd9b889cc14d0c1ca10543
3nzo <3nzo@mbp-de-di.home>
2024-03-13 12:18:55 +0100

    modification php

--- contact.php
+++ contact.php
@@ -8,7 +8,7 @@ $subject = "Prise de contact : $nom ";
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // Vérifie si des champs sont vides ou si l'email n'est pas valide
     if (empty($nom) || !filter_var($email, FILTER_VALIDATE_EMAIL) || empty($message)) {
-        echo "<script>alert('Veuillez remplir correctement tous les champs.'); window.location.href = '/index.html';</script>";
+        echo "<script>alert('Veuillez remplir correctement tous les champs.'); window.location.href = 'https://enzodigiovanni.fr/';</script>";
     } else {
         $to = 'digiovannienzo1@gmail.com';
         $from = 'no-reply@enzodigiovanni.fr';
@@ -20,7 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             header('Location: /success-page.php');
             exit;
         } else {
-            echo "<script>alert('Erreur lors de l\'envoi du message.'); window.location.href = '/contact-form.php';</script>";
+            echo "<script>alert('Erreur lors de l\'envoi du message.'); window.location.href = 'https://enzodigiovanni.fr/';</script>";
         }
     }
 }
