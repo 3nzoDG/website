cb9951055048e3bd9e2b4e1d9c597b725ebe2e9e
3nzo <3nzo@mbp-de-di.home>
2024-03-14 13:07:56 +0100

    Modification folder/ IMG png to WEBP/Avif / HTML modification/ Code minify

---
+++ src/php/contact.php
@@ -0,0 +1,28 @@
+<?php
+
+$nom = $_POST['full-name'] ?? '';
+$email = $_POST['email'] ?? '';
+$message = $_POST['message'] ?? '';
+$subject = "Prise de contact : $nom ";
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Vérifie si des champs sont vides ou si l'email n'est pas valide
+    if (empty($nom) || !filter_var($email, FILTER_VALIDATE_EMAIL) || empty($message)) {
+        echo "<script>alert('Veuillez remplir correctement tous les champs.'); window.location.href = 'https://enzodigiovanni.fr/';</script>";
+    } else {
+        $to = 'digiovannienzo1@gmail.com';
+        $from = 'no-reply@enzodigiovanni.fr';
+        $headers = "From: $email\r\nReply-To: $email\r\nX-Mailer: PHP/" . phpversion();
+        $content = "Nom: $nom\nEmail: $email\nMessage: $message";
+
+        if (mail($to, $subject, $content, $headers)) {
+            // Redirection après l'envoi réussi
+            header('Location: /success-page.php');
+            exit;
+        } else {
+            echo "<script>alert('Erreur lors de l\'envoi du message.'); window.location.href = 'https://enzodigiovanni.fr/';</script>";
+        }
+    }
+}
+
+
